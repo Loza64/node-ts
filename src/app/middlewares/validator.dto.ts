8e3bc864157ed325a535d8147e61df72b7c8cf7d
Roberto Loza <robertoloxa88@gmail.com>
2025-05-08 08:47:32 -0600

    implement meta-data

--- src/app/middlewares/validator.dto.ts
+++ src/app/middlewares/validator.dto.ts
@@ -2,6 +2,7 @@ import { ClassConstructor } from "class-transformer/types/interfaces";
 import { NextFunction, Request, Response } from "express";
 import { plainToClass } from "class-transformer";
 import { validate } from "class-validator";
+import 'reflect-metadata';
 
 export default function validateDTO<T extends object>(dtoClass: ClassConstructor<T>) {
 
