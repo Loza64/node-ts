cc6556db5ac625825b6656ff2ab4a8824e91a1f4
loza.dev <robertoloxa88@gmail.com>
2025-10-06 07:32:51 -0600

    update validation dto

--- src/app/middlewares/validator.dto.ts
+++ src/app/middlewares/validator.dto.ts
@@ -1,22 +1,25 @@
 import { ClassConstructor } from "class-transformer/types/interfaces";
 import { NextFunction, Request, Response } from "express";
-import { plainToClass } from "class-transformer";
+import { plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
 import 'reflect-metadata';
 
 export default function validateDTO<T extends object>(dtoClass: ClassConstructor<T>) {
-
     return async (req: Request, res: Response, next: NextFunction) => {
 
-        const dto = plainToClass(dtoClass, req.body);
-        const errors = await validate(dto);
-        
+        const dto = plainToInstance(dtoClass, req.body);
+
+        const errors = await validate(dto, { whitelist: true, forbidNonWhitelisted: true });
+
         if (errors.length > 0) {
-            res.status(400).json({ errors })
-            return;
+            const messages = errors.flatMap(err => Object.values(err.constraints ?? {}));
+            return res.status(400).json({
+                status: "error",
+                message: "validation error",
+                errors: messages,
+            });
         }
 
         next();
-    }
-
+    };
 }
