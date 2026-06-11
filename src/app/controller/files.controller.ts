ea9c81e27afdd4308a703e1521a18cad9c544dec
loza.dev <robertoloxa88@gmail.com>
2026-06-10 22:58:43 -0600

    fix bug to files endpint

--- src/app/controller/files.controller.ts
+++ src/app/controller/files.controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
 export const uploadFilesCloud = (req: Request, res: Response, _next: NextFunction): void => {
-    const files = req.files ?? [];
+  const files = (req.files as Express.Multer.File[]) ?? [];
 
-    res.status(200).json({
-        total: files.length,
-        names: files.map(f => f.originalname),
-        sizes: files.map(f => f.size),
-        mimetypes: files.map(f => f.mimetype),
-    });
+  res.status(200).json({
+    total: files.length,
+    names: files.map(f => f.originalname),
+    sizes: files.map(f => f.size),
+    mimetypes: files.map(f => f.mimetype),
+  });
 };
 
 export const createUser = (req: Request, res: Response, _next: NextFunction): void => {
-    // req.body ya fue validado por validateDTO(CreateUserDto)
-    res.status(201).json({ message: "User created", data: req.body });
+  // req.body ya fue validado por validateDTO(CreateUserDto)
+  res.status(201).json({ message: "User created", data: req.body });
 };
